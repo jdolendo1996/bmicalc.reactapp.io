2b5988e6993f0f42000ac45a1dc806399c9a6d80
unknown <areeshdolendo@gmail.com>
2020-10-23 22:07:27 +0700

    Updates

---
+++ precache-manifest.420532a60fc2fb46aafd9ba8343e6eb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96258e213a0308608996aa47c76f9e43",
+    "url": "/bmicalc.reactapp.io/index.html"
+  },
+  {
+    "revision": "5a18874752e9fa8df6ce",
+    "url": "/bmicalc.reactapp.io/static/css/2.65c1222a.chunk.css"
+  },
+  {
+    "revision": "2af089143417214564c9",
+    "url": "/bmicalc.reactapp.io/static/css/main.1879a869.chunk.css"
+  },
+  {
+    "revision": "5a18874752e9fa8df6ce",
+    "url": "/bmicalc.reactapp.io/static/js/2.3ae38996.chunk.js"
+  },
+  {
+    "revision": "55401ebad3550eaa03e4121f76030449",
+    "url": "/bmicalc.reactapp.io/static/js/2.3ae38996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af089143417214564c9",
+    "url": "/bmicalc.reactapp.io/static/js/main.7be046a8.chunk.js"
+  },
+  {
+    "revision": "6527db151e0ca16ea942",
+    "url": "/bmicalc.reactapp.io/static/js/runtime-main.a1191227.js"
+  },
+  {
+    "revision": "322db6516d359acbd66f5c11cde83443",
+    "url": "/bmicalc.reactapp.io/static/media/bg_image1.322db651.jpg"
+  }
+]);
